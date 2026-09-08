0b20e36ff8eb8b8dd93aea6b695a7759e93ee8d7
Shiv <present.shiv@gmail.com>
2026-09-09 00:33:49 +0530

    Done with bird animation

--- core.py
+++ core.py
@@ -19,8 +19,34 @@ pygame.display.set_caption("Fallpy Bird")
 # Images 
 bg = pygame.image.load("/home/Projects/Flappy_bird/images/bglong.png")
 ground = pygame.image.load("/home/Projects/Flappy_bird/images/ground.png")
+class Bird(pygame.sprite.Sprite):
+    def __init__(self , x , y ):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1 , 4):
+            img = pygame.image.load(f'/home/Projects/Flappy_bird/images/bird{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x , y]
 
+    def update(self):
+        #Handle animation
+        self.counter += 1
+        flap_cooldown = 5
 
+        if self.counter > flap_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
+
+bird_group = pygame.sprite.Group()
+flappy = Bird(100 , int(screen_height / 2))
+bird_group.add(flappy)
 #flag
 running = True
 
@@ -28,6 +54,8 @@ running = True
 while running:
     clock.tick(fps)
     screen.blit(bg , (0 , 0 ))
+    bird_group.draw(screen)
+    bird_group.update()
     screen.blit(ground , (ground_scroll , 768))
     ground_scroll -= scroll_speed
     if abs(ground_scroll) > 35:
